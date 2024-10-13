91dc6f0de800044f8ac8ab1e05beb11ccac26525
Geordy <ggeordymm@gmail.com>
2024-10-12 22:16:51 -0500

    some change

--- src/main/java/com/plazoleta/plazoleta/domain/model/pagination/PaginationCustom.java
+++ src/main/java/com/plazoleta/plazoleta/domain/model/pagination/PaginationCustom.java
@@ -1,9 +1,16 @@
 package com.plazoleta.plazoleta.domain.model.pagination;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
 
+@Getter
+@Setter
 public class PaginationCustom<T> {
     private List<T> content;
     private int pageNumber;
@@ -28,52 +35,4 @@ public class PaginationCustom<T> {
         this(content, pageNumber, 0, 0, 0, true); // Constructor simplificado
     }
 
-    // Getters y Setters
-    public List<T> getContent() {
-        return content;
-    }
-
-    public void setContent(List<T> content) {
-        this.content = content;
-    }
-
-    public int getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(int pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public long getTotalElements() {
-        return totalElements;
-    }
-
-    public void setTotalElements(long totalElements) {
-        this.totalElements = totalElements;
-    }
-
-    public int getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(int totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public boolean isLast() {
-        return isLast;
-    }
-
-    public void setLast(boolean isLast) {
-        this.isLast = isLast;
-    }
 }
